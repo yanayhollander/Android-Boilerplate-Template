e61149b3cf36177e0bb9f51825e866d6ee164dc8
Yanay Hollander <yanay.hollander@clearme.com>
2024-01-09 14:56:29 +0200

    add preview for the navbar

--- app/src/main/java/com/hollander/template/presentation/composables/AppScaffold.kt
+++ app/src/main/java/com/hollander/template/presentation/composables/AppScaffold.kt
@@ -1,11 +1,12 @@
 package com.hollander.template.presentation.composables
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -13,12 +14,11 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hollander.template.R
@@ -66,6 +66,36 @@ fun AppScaffold(
     {
         Column(modifier = Modifier.padding(it)) {
             content()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppScaffoldPreview() {
+    AppScaffold(title = "Title") {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.secondary)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            (1..100).map { Text(text = "Text ${it}") }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppScaffoldWithBackArrowPreview() {
+    AppScaffold(title = "Title with back arrow", showBackButton = true) {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.secondary)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            (1..100).map { Text(text = "Text ${it}") }
         }
     }
 }
